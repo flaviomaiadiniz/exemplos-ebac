7454905539b75a930c4f82293fbd1ef159325fcc
flaviomaiadiniz <fmaiadiniz@gmail.com>
2023-06-20 22:04:57 -0300

    meu commit

---
+++ Carro.java
@@ -0,0 +1,42 @@
+package br.com.flavio;
+
+public class Carro {
+	
+	private String nomeCarro;
+	private String marcaCarro;
+	private int anoCarro;
+	private float valorCarro;
+	
+	
+	public String getNomeCarro() {
+		return nomeCarro;
+	}
+	public void setNomeCarro(String nomeCarro) {
+		this.nomeCarro = nomeCarro;
+	}
+	public String getMarcaCarro() {
+		return marcaCarro;
+	}
+	public void setMarcaCarro(String marcaCarro) {
+		this.marcaCarro = marcaCarro;
+	}
+	public int getAnoCarro() {
+		return anoCarro;
+	}
+	public void setAnoCarro(int anoCarro) {
+		this.anoCarro = anoCarro;
+	}
+	public float getValorCarro() {
+		return valorCarro;
+	}
+	public void setValorCarro(float valorCarro) {
+		this.valorCarro = valorCarro;
+	}
+	
+	
+	
+	/**
+	 * @author Flávio Maia
+	 */
+	
+}
